e4fd9eab95e9f95400911575986f7566a3b95deb
harrnish <harrnish@gmail.com>
2023-02-23 21:24:57 -0500

    Setting up coming soon page. 🔥

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header">
+          <p>
+            THE collection is on its way, soon to showcase the coolest designs
+            that'll take your breath away.
+          </p>
+        </div>
+        <div className="footer">
+          <p>
+            ALL RIGHTS RESERVED &copy; ZEN-lIKE <br />
+            <span>stay zen and keep calm</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
