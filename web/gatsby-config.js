ee6532300bb5af9a79dc231233ecda87ef3d9899
harrnish <harrnish@gmail.com>
2023-02-25 16:31:21 -0500

    Project setup updates.

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,10 +1,10 @@
-require('dotenv').config('./.env');
+require("dotenv").config("./.env");
 
 module.exports = {
   siteMetadata: {
-    title: `TechHub-blog`,
-    siteUrl: `https://techhub-blog.netlify.com`,
-    description: `TechHub Blog is a platform for latest technology news and updates.`,
+    title: `zen-like-website`,
+    siteUrl: `https://zen-like.website`,
+    description: ``,
   },
   plugins: [],
 };
